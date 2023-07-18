ecc7192cb10034aeee55dad8bce04c65c927ff2a
Javier Collao <javier.collao@usach.cl>
2023-07-18 15:06:36 -0400

    Add Hash Router

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import ReactDOM from 'react-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Use HashRouter and Routes
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/styles/kino.css';
+import './styles/kino.css';
 import Kino from './routes/Kino';
 import Generator from './routes/Generator';
 import DataProvider from './context/DataProvider';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Kino />,
-    errorElement: <h1>Error</h1>
-  },
-  {
-    path: "/generator",
-    element: <Generator />
-  }
-]);
+const App = () => {
+  return (
+    <React.StrictMode>
+      <DataProvider>
+        <HashRouter>
+          <Routes> {/* Use Routes instead of Switch */}
+            <Route path="/" element={<Kino />} /> {/* Use "element" instead of "component" */}
+            <Route path="/generator" element={<Generator />} /> {/* Use "element" instead of "component" */}
+            <Route path="*" element={<h1>Error</h1>} /> {/* Fallback route for 404 */}
+          </Routes>
+        </HashRouter>
+      </DataProvider>
+    </React.StrictMode>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <DataProvider>
-      <RouterProvider router={router} />
-    </DataProvider>
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
